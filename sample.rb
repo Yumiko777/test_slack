1dcb0db8b212dd4f4473957118a9b14ca6e6336f
Yumiko <wakinghira73@gmail.com>
2021-06-03 00:02:29 +0900

    Clockwork

--- sample.rb
+++ sample.rb
@@ -1,4 +1,12 @@
 require "slack-notify"
+require 'clockwork'
 
-sample = SlackNotify::Client.new(webhook_url: ENV['WEBHOOK'])
-sample.notify("Hello There!")
+include Clockwork
+
+handler do |job|
+  puts "Running #{job}"
+  client = SlackNotify::Client.new(webhook_url: "https://hooks.slack.com/services/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")
+  client.notify("Hello There!")
+end
+
+every(3.minutes, 'less.frequent.job')
